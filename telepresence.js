d4d87cbf265a27b798cd393ecc77d03e627a8c78
Mark Silliman <mwsilliman@gmail.com>
2015-03-09 17:07:11 -0700

    local domain

--- telepresence.js
+++ telepresence.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $(".activestate").removeClass("activestate");
         $(this).addClass("activestate");
 
-        var domain = "http://ec2-52-11-246-12.us-west-2.compute.amazonaws.com"; //change this to your public dns
+        var domain = "http://" + document.domain; //change this to your public dns
         var script = "/api.php?save=" + $(this).attr("id");
 
         //save the action to the server
